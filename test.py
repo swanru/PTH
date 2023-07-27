d885c42f775d318c0b4f13cea9ce86928e7599ab
swanru <selwanriaudyu48@gmail.com>
2023-07-27 17:27:58 +0700

    Adding files and import openxyl

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
 import pandas as pd
-print("test")
+from openpyxl import load_workbook
+wb = load_workbook(filename='2021.xlsx')
+sr = wb['Sheet1']
+data = pd.DataFrame(sr.values)
+d = data
+d[1:5][[0,1,2,3,4,5]]
+d.columns = ['Daerah','Positif','Sembuh','Dirawat','Meninggal','BULAN']
+april = d[d.BULAN == 'APRIL'].reset_index(drop=True)
+april = april[['Daerah','Positif','Meninggal']]
+april
